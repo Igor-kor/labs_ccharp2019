c08326fa394fc2c11cf86ea0bf7a1b0edb7d4800
ikor <igor_korenovsk@mail.ru>
2019-11-20 01:55:37 +0300

    сделал лаб7

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Лабораторная работа 7
+ * 
+ * Рекурсия
+ * 
+ * Создать рекурсивный метод, реализующий один из следующих алгоритмов обработки последовательности чисел. 
+ * 1) Последовательность Фибоначчи 
+ */
+
+namespace lab7
+{
+    class Program
+    {
+        static void fibonachi(int x, int y, int max)
+        {
+            if (y > max) return;
+            Console.WriteLine(y);
+            fibonachi(y, x + y, max);
+        }
+
+        static void Main(string[] args)
+        {
+            fibonachi(1, 1, 1000);
+            Console.ReadKey();
+        }
+
+    }
+}
